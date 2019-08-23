67b206d7416df0ab278f0d1cd961b8a65bd89b96
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2019-08-22 23:29:54 -0300

    ok

--- utils/index.js
+++ utils/index.js
@@ -2,10 +2,29 @@
   utils
     /time (delay, isToday, tomorow, rangeDate, isToday, isBefore, isAfter, daysDiff)
     /math (between,range, toString(stringfy), round, round2)
-    /array (toObject, fromObject, toDimensional, getFieldMap, avarage)
+    /array (flatMap, toObject, fromObject, toDimensional, getFieldMap, avarage)
     /functions (not, pipeline, promisify)
+    / Maybe
 */
 
+// !!!!! Os MayBe e similares sao importants para garantirmos certa validação de dados
+
+
+// rejeitar o cathc  Promise.reject("Não foi possível obter as notas fiscais")
+
+/* 
+
+
+  listAll() {
+    return fetch(API)
+      .then(handleStatus)
+      .catch(err => {
+        console.log(err);
+        return Promise.reject("Não foi possível obter as notas fiscais");
+      });
+  },
+  
+  */
 const unary = fn => (...args) => fn(args[0]);
 const isNull = x => x === null || x === undefined;
 const not = fn => (...args) => !fn(...args);
